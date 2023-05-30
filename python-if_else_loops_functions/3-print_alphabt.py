a384601c4695f6182ecd5b0896bcb65c54a85459
nimrod02 <letrun.a@gmail.com>
2023-05-30 13:58:41 +0200

    add task 3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for letter in range(ord('a'), ord('z')+1):
-    if letter != ord("e") and ord("q"):
+    if letter != ord("e") and letter != ord("q"):
         print("{}".format(chr(letter)), end="")
