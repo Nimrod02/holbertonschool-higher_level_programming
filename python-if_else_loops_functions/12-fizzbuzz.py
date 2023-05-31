6ccc0bada08cd3a14bcde92e5076e8311e8d4ccb
nimrod02 <letrun.a@gmail.com>
2023-05-31 14:04:04 +0200

    add task 12

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for f in range(1, 101):
+        if f % 15 == 0:
+            print("FizzBuzz", end="")
+        elif f % 5 == 0:
+            print("Buzz", end="")
+        elif f % 3 == 0:
+            print("Fizz", end="")
+        else:
+            print("{}".format(f), end="")
+        if f < 100:
+            print(" ", end="")
