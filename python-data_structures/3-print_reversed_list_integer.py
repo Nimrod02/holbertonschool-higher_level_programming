959374ee83dfffcbb68f693b8fae55163b2ba349
nimrod02 <letrun.a@gmail.com>
2023-06-05 11:16:35 +0200

    fix task 3 with codition

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for index in reversed(my_list):
-        print("{:d}".format(index))
-    if index < 0 or index >= len(my_list):
-        return my_list
+    if my_list is not None:
+        for index in reversed(my_list):
+            print("{:d}".format(index))
