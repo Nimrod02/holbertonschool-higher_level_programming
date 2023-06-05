4ca49d1124e00de0022eb637ab17fcc411d88e7b
nimrod02 <letrun.a@gmail.com>
2023-06-05 15:39:00 +0200

    fix task 9 with new indentation

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,8 +2,11 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
+
     max_value = my_list[0]
+
     for num in my_list:
         if num > max_value:
             max_value = num
-            return max_value
+
+    return max_value
