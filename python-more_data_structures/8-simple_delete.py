b1eb6b0a5d4b97f58ae8d2550d587ae96e7f2f56
nimrod02 <letrun.a@gmail.com>
2023-06-06 14:11:14 +0200

    update with a new condition

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -3,5 +3,5 @@ def simple_delete(a_dictionary, key=""):
     if key is not None and a_dictionary is not None:
         return a_dictionary
 
-    a_dictionary.pop(key)
+    del a_dictionary[key]
     return a_dictionary
