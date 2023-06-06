26a80ca5600a69fe555dfc932954da74e458bec4
nimrod02 <letrun.a@gmail.com>
2023-06-06 13:25:52 +0200

    fix task 4 with new syntax

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    res = set(set_1)
-    for s in set_1:
-        res = res.symmetric_difference(set_2)
+    res = set_1.symmetric_difference(set_2)
     return res
