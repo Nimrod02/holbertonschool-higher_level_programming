8fbb57531b8d2378f5527896c635d195d5c65296
nimrod02 <letrun.a@gmail.com>
2023-06-06 15:31:26 +0200

    add task 12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string or roman_string is None:
+        return None
+
+    rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    rom_int = 0
+    prev_value = 0
+
+    for char in reversed(roman_string):
+        value = rom_val.get(char, 0)
+        if value >= prev_value:
+            rom_int += value
+        else:
+            rom_int -= value
+        prev_value = value
+
+    return rom_int
