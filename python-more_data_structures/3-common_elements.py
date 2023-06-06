2bbb26f9decada3acbbe41c64caeeb9eb6b4764d
nimrod02 <letrun.a@gmail.com>
2023-06-06 13:08:35 +0200

    fix task 3 with a list

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    a_set = set(set_1)
-    b_set = set(set_2)
-    if (a_set & b_set):
-        print(a_set & b_set)
+    result = [index for index in set_1 if index in set_2]
+    return result
