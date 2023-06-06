20fde140fab19943da6873429477a9fad770ffde
nimrod02 <letrun.a@gmail.com>
2023-06-06 11:06:57 +0200

    add task 2

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    uniq_value = set(my_list)
+
+    for num in my_list:
+        uniq_value.add(num)
+
+        uniq_sum = sum(uniq_value)
+
+    return uniq_sum
