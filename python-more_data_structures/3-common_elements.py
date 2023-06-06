e34538f5e43dd6bad0aee309ef175eb861b9bf2b
nimrod02 <letrun.a@gmail.com>
2023-06-06 13:05:53 +0200

    fix task 3 with set

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    for char in set_1:
-        for same in set_2:
-            if char == same:
-                return char
+    a_set = set(set_1)
+    b_set = set(set_2)
+    if (a_set & b_set):
+        print(a_set & b_set)
