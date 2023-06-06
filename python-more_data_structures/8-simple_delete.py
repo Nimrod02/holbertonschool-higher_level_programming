ee66c887ad77a2cb5bba4b8d6778a52bfc455eff
nimrod02 <letrun.a@gmail.com>
2023-06-06 14:12:17 +0200

    fix with new syntax and indentation

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if key is not None and a_dictionary is not None:
-        return a_dictionary
-
-    del a_dictionary[key]
+    if key in a_dictionary:
+        del a_dictionary[key]
     return a_dictionary
