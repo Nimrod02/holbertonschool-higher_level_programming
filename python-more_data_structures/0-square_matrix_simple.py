6dbeb10598fc405e0c14c8ac502ebc66d07363aa
nimrod02 <letrun.a@gmail.com>
2023-06-06 10:16:27 +0200

    fix task 0 with a codition for the matrix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    if not matrix:
+        return None
+    
     new_matrix = [[0 for _ in range(len(matrix[0]))]
                   for _ in range(len(matrix))]
 
