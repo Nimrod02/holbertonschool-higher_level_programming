e3636c48b1bf2957e51db37b9ac49d91a74c17b9
nimrod02 <letrun.a@gmail.com>
2023-06-07 13:41:11 +0200

    fix task 4 remove the spaces

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -10,16 +10,12 @@ def list_division(my_list_1, my_list_2, list_length):
         except TypeError:
             print("wrong type")
             div_result = 0
-
         except ZeroDivisionError:
             print("division by 0")
             div_result = 0
-
         except IndexError:
             print("out of range")
             div_result = 0
-
         finally:
             result.append(div_result)
-
     return result
