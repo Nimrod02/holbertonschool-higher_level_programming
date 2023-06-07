1f85443bca6ecf7aa86f1dfac6ca421e68f11719
nimrod02 <letrun.a@gmail.com>
2023-06-07 11:00:51 +0200

    fix task 2 with new syntax

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,10 @@
-#!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        count = 0
-        for index in range(x):
-            if isinstance(my_list[index], int):
-                print("{:d}".format(my_list[index]), end="")
-                count += 1
-        print()
-        return count
-
-    except IndexError:
-        print()
-        return count
+    count = 0
+    for index in range(x):
+        try:
+            print("{:d}".format(my_list[index]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            continue
+    print()
+    return count
