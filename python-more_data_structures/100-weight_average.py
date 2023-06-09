dfe1023fe07ebeba3b7f9585d4c307f01cfb8957
nimrod02 <letrun.a@gmail.com>
2023-06-09 15:13:59 +0200

    fix task 100 indentation probleme

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     if my_list is not None:
-            res = 0
-            recurrence = 0
-            for index in my_list:
-                (weight, occurrence) = index
-                res += (weight * occurrence)
-                recurrence += occurrence
-                return (res / recurrence) if recurrence > 0 else 0
-            else:
-                return 0
+        res = 0
+        recurrence = 0
+        for index in my_list:
+            (weight, occurrence) = index
+            res += (weight * occurrence)
+            recurrence += occurrence
+        return (res / recurrence) if recurrence > 0 else 0
+    else:
+        return 0
