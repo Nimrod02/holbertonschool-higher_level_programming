a0d679c044d4b60abaeff925d116a070f1b106ae
nimrod02 <letrun.a@gmail.com>
2023-06-12 11:18:27 +0200

    add task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    module 1-square
+"""
+
+
+class Square:
+    """
+    class Square definition
+
+    Args:
+        size : size of square
+    """
+    def __init__(self, size):
+        """
+    Args:
+        __size: call the size of square
+    """
+        self.__size = size
