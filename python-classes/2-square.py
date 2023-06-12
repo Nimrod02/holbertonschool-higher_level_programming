15437f8228aebd1b2d348bc25529f69753e8d8d2
nimrod02 <letrun.a@gmail.com>
2023-06-12 12:37:23 +0200

    add task 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    module 2-square
+"""
+
+
+class Square:
+    """
+    class Square definition
+
+    Args:
+        size : size of square
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
