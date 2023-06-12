d625d3791606d5ec3823a8493b880005bf5e89c1
nimrod02 <letrun.a@gmail.com>
2023-06-12 16:49:44 +0200

    fix task 6 for like the hundred Fucking time

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -47,5 +47,8 @@ class Square:
     def my_print(self):
         if self.size == 0:
             print()
+        else:
+            for _ in range(self.__position[1]):
+                print()
         for _ in range(self.__size):
             print(' ' * self.__position[0] + "#" * self.__size)
