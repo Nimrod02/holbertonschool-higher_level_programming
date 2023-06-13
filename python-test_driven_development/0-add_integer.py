92ad393f5f98b89093c892b1aba847a6dc8e9bc6
nimrod02 <letrun.a@gmail.com>
2023-06-13 10:40:21 +0200

    fix task 0 and fix dir test

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -9,8 +9,7 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    if a is isinstance(int, float) or b is isinstance(int, float):
+    if isinstance(a, float) or isinstance(b, float):
         a = int(a)
         b = int(b)
     return int(a + b)
-
