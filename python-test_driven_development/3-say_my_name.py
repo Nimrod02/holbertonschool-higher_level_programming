4cc4bbd04ce05e61c12b89aad3c116b740270fa8
nimrod02 <letrun.a@gmail.com>
2023-06-14 13:51:20 +0200

    update task 3 with comment

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -5,6 +5,12 @@ module 3 - say my name
 
 
 def say_my_name(first_name, last_name=""):
+    """say_my_name
+
+    Args:
+        first_name: first name
+        last_name: last_name
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
