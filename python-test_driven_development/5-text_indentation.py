2c97c3de8fc92a693bd596d23f16052bf9485a97
nimrod02 <letrun.a@gmail.com>
2023-06-17 12:03:28 +0200

    fix all previous test and add task 5 and test 5

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+ module 5 - text_indedation
+"""
+
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    for delimiter in ".?:":
+       text = text.replace(delimiter, delimiter + "\n\n")
+    each_lines = [lines.strip(' ') for lines in text.split('\n')]
+    new_line = "\n".join(each_lines)
+    print(new_line, end="")
