1157238389dea94bc9f09f15b9c344ec0c1f3d63
nimrod02 <letrun.a@gmail.com>
2023-06-18 13:15:30 +0200

    test new thing

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,49 +1,48 @@
 #!/usr/bin/python3
-"""Unittest for max_integer([..])
+"""
+Unittest for max_integer([..])
 """
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
 
-    """test class for the max_integer function
+    """
+    run test with python3 -m unittest -v tests.6-max_integer_test
     """
 
-    def test_docstring(self):
-        doc = __import__('6-max_integer').__doc__
-        self.assertTrue(len(doc) > 1)
+    def test_module_docstring(self):
+        moduleDoc = __import__('6-max_integer').__doc__
+        self.assertTrue(len(moduleDoc) > 1)
 
-    def test_function(self):
-        function = __import__("6-max_integer").max_integer.__doc__
-        self.assertTrue(len(function) > 1)
+    def test_function_docstring(self):
+        functionDoc = __import__('6-max_integer').max_integer.__doc__
+        self.assertTrue(len(functionDoc) > 1)
 
-    def test_max_int_float(self):
+    def test_signed_ints_and_floats(self):
         self.assertEqual(max_integer([0]), 0)
-        self.assertEqual(max_integer([1, 2, 3]), 3)
-        self.assertEqual(max_integer([0, -1, 2]), 2)
-        self.assertEqual(max_integer([0, 1, 2, 3, 4]), 4)
-        self.assertEqual(max_integer([0.0]), 0.0)
-        self.assertEqual(max_integer([1.0, 2.0, 3.0]), 3.0)
-        self.assertEqual(max_integer([0.0, -1.0]), 0.0)
-        self.assertEqual(max_integer([0.0, 1.0, 2.0, 3.0, 4.0]), 4.0)
-        self.assertEqual(max_integer([{1, 9}, {2, 3}, {1}, {2}]), {1, 9})
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([1, 2, 3, -4]), 3)
+        self.assertEqual(max_integer([-1.5, -2.5]), -1.5)
+        self.assertEqual(max_integer([10, -10, 10]), 10)
+        self.assertEqual(max_integer([{1, 9}, {2}, {3}]), {1, 9})
+
+    def test_list_of_strings(self):
+        self.assertEqual(max_integer("6789"), '9')
+        self.assertEqual(max_integer("abcxyz"), 'z')
+        self.assertEqual(max_integer(['a', 'b', 'c', 'x', 'y', 'z']), 'z')
+        self.assertEqual(max_integer(["abc", 'x']), 'x')
 
     def test_lists(self):
-        self.assertEqual(max_integer([[1, 2], [3, 4], [5, 6]]), [5, 6])
+        self.assertEqual(max_integer([[1, 2], [1, 3]]), [1, 3])
 
-    def test_string(self):
-        self.assertEqual(max_integer("abcd"), "d")
-        self.assertEqual(max_integer("123456789"), "9")
-        self.assertEqual(max_integer(["a", "b", "c", "d", "e"]), "e")
-        self.assertEqual(max_integer(["abdc", "d"]), "d")
-
-    def test_other(self):
+    def test_other_sequences(self):
         with self.assertRaises(TypeError):
-            max_integer({1, 2}, {3, 4, 5, 6})
+            max_integer({1, 2}, {3, 4, 5})
         with self.assertRaises(TypeError):
             max_integer({1, 2, 3, 4, 5})
         with self.assertRaises(TypeError):
-            max_integer([-9, "str", -5.5, 10, 11])
+            max_integer([-10, 0.5, "str", {1, 2}])
         with self.assertRaises(TypeError):
             max_integer([None, True])
 
@@ -52,5 +51,5 @@ class TestMaxInteger(unittest.TestCase):
         self.assertIsNone(max_integer(), None)
         self.assertIsNone(max_integer([None]), None)
 
-    if __name__ == "__main__":
-        unittest.main()
+if __name__ == "__main__":
+    unittest.main()
