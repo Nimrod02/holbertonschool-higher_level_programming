3e2065dccb94b33837ad6adc0cba3a76324d2e92
nimrod02 <letrun.a@gmail.com>
2023-06-19 16:01:01 +0200

    add comment to task 0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
-
-
+"""
+module 0-lookup
+"""
 def lookup(obj):
+    """lookup
+
+    Args:
+        obj (str): contains all string for the object
+
+    Returns:
+        dir(obj): return all the possible string in the object
+    """
     return dir(obj)
