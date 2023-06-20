781173831891707b13fffc08f52c8778a25c6d30
nimrod02 <letrun.a@gmail.com>
2023-06-20 10:47:21 +0200

    ad comment to task 2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
-
+"""
+module 2-is_same_class
+"""
 def is_same_class(obj, a_class):
+    """is_same_class
+
+    Args:
+        obj (int): comparison1
+        a_class (int): comparison2
+
+    Returns:
+        _type_: the result of the comparison
+    """
     return True if type(obj) is a_class else False
