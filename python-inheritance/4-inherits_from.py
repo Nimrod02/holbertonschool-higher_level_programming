e5a1689f0b33835f73da268ab98dad41706987ce
nimrod02 <letrun.a@gmail.com>
2023-06-20 14:17:19 +0200

    add task 4 for test checker

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+module 4-inherits_from
+"""
+
+
+def inherits_from(obj, a_class):
+    """inherits_from
+
+    Args:
+        obj (int): comparison1
+        a_class (int): comparison2
+
+    Returns:
+        _type_: the result of the comparison
+    """
+    return True if issubclass(type(obj), a_class) else False
