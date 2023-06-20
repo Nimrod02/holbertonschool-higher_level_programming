db5161c69d27700fac56987a6c85109ffb0022a0
nimrod02 <letrun.a@gmail.com>
2023-06-20 13:52:26 +0200

    add task 3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+module 3-is_kind_of_class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """is_kind_of_class
+
+    Args:
+        obj (int): comparison1
+        a_class (int): comparison2
+
+    Returns:
+        _type_: the result of the comparison
+    """
+    return True if type(obj) is a_class or issubclass(type(obj), a_class) else False
