37432f60fdbba40adfb99badb7ff798a0d7b229f
nimrod02 <letrun.a@gmail.com>
2023-06-20 14:43:32 +0200

    add task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+module 6-base_geometry
+"""
+
+
+class BaseGeometry:
+    """
+    class baseGeometry
+    """
+    def area(self):
+        raise TypeError("area() is not implemented")
