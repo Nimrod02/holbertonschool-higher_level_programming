77a146d5bc8172baf6cbfcec7909a24bbea64b45
nimrod02 <letrun.a@gmail.com>
2023-06-21 13:52:42 +0200

    add file 1 write_file to the project input/output

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+module 1-write_file
+"""
+
+
+def write_file(filename="", text=""):
+    """write_file
+
+    Args:
+        filename (str)
+        text (str)
+    """
+    with open(filename, 'w', encoding="utf-8") as file:
+        file.write(text)
+
