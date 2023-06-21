7a36bf2ba93a1a73818b9df013b44dc9e2b897c7
nimrod02 <letrun.a@gmail.com>
2023-06-21 13:33:54 +0200

    add pycodestyle to task 0-read_file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,11 +2,13 @@
 """
 module 0-read_file
 """
+
+
 def read_file(filename=""):
     """read_file
 
     Args:
-        filename (str, optional): contain the file of the test. Defaults to read_file"".
+        filename (str, optional): contain the file of the test.
     """
     with open(filename) as f:
         for lines in f:
