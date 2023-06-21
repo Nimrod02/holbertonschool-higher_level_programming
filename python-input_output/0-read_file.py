30472abcce9c6c4b46f734305644fb0a3352bf9b
nimrod02 <letrun.a@gmail.com>
2023-06-21 13:32:46 +0200

    add comment to the task 0-read_file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+module 0-read_file
+"""
 def read_file(filename=""):
+    """read_file
+
+    Args:
+        filename (str, optional): contain the file of the test. Defaults to read_file"".
+    """
     with open(filename) as f:
         for lines in f:
             print(lines, end="")
