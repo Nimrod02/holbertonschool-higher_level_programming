e0dc98aa91109d8f5e745828736df5ce7f10a445
nimrod02 <letrun.a@gmail.com>
2023-06-21 09:23:40 +0200

    fix position of the integer_validator

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 """
 module 8-rectangle
 """
@@ -12,7 +11,7 @@ class Square(Rectangle):
         class Square
     """
     def __init__(self, size):
+        self.integer_validator("size", size)
+
         super().__init__(size, size)
         self.__size = size
-
-        self.integer_validator("size", self.__size)
