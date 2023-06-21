50a45445cfc1547906249a24c2949c1a72470b78
nimrod02 <letrun.a@gmail.com>
2023-06-21 08:58:42 +0200

    add task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+module 8-rectangle
+"""
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """
+        class Square
+    """
+    def __init__(self, size):
+        super().__init__(size, size)
+        self.__size = size
+
+        self.integer_validator("size", self.__size)
