62404c5e0e4ded75d5b9727f5d9e1490128f9cd4
nimrod02 <letrun.a@gmail.com>
2023-06-22 13:50:28 +0200

    add task 8

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+module 8-class_to_json
+"""
+
+import json
+
+
+def class_to_json(obj):
+    """class_to_json
+
+    Args:
+        obj (dict): dictionary contain all the requirment for the task
+    """
+    return obj.__dict__
