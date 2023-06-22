7d3ada9ce94646c1bf5424594380f338b7fdc072
nimrod02 <letrun.a@gmail.com>
2023-06-22 17:12:03 +0200

    fix test file

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -26,7 +26,7 @@ class BaseGeometry:
             -value: interger to ckeck
         """
 
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
