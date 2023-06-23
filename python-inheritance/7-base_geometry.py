e4e6ffc3d2e7d3e0c8807cc7b18903bd404ff451
nimrod02 <letrun.a@gmail.com>
2023-06-23 08:53:56 +0200

    reset test and task 7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,32 +1,16 @@
 #!/usr/bin/python3
-"""
-module 6-base_geometry
-"""
+"""Write a class BaseGeometry (based on 5-base_geometry.py)."""
 
 
 class BaseGeometry:
-    """
-    class baseGeometry
-    """
-    def area(self):
-        """area
+    """class geometry"""
 
-        Raises:
-            Exception: raise an error if execption
-        """
-        raise Exception("area() is not implemented")
+    def area(self):
+        """return exception of area"""
+        raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
-        """
-        Resume:
-            Verify if value is a interger
-
-        Args:
-            -name: always string, the name of Geometry.
-            -value: interger to ckeck
-        """
-
         if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError('{} must be an integer'.format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
