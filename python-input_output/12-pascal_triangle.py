f4e9deff7170c780a73ece451280a10c12922892
nimrod02 <letrun.a@gmail.com>
2023-06-23 13:59:56 +0200

    add task 12

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 
-
 def pascal_triangle(n):
-    prev_line = [1]
+    triangle = []
+    current_row = [1]
     if n <= 0:
-        return []
+        return triangle
+    triangle.append(current_row)
 
-    for rows in range(n):
-        new_line = prev_line + [1]
-        for columns in range(len(prev_line) - 1):
-            new_line[columns + 1] = prev_line[columns] + prev_line[columns + 1]
-        prev_line = new_line
-        print(new_line)
+    for rows in range(1, n):
+        new_row = current_row + [1]
+        for column in range(len(current_row) - 1):
+            new_row[column + 1] = current_row[column] + current_row[column + 1]
+        current_row = new_row
+
+        triangle.append(current_row)
+
+    return triangle
