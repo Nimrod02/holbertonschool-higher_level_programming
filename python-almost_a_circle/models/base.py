66f62cd1e880c0f4c5cbeb1a449a09d2581d8540
nimrod02 <letrun.a@gmail.com>
2023-06-26 10:47:09 +0200

    add new project python-almost_a_circle

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+module base
+"""
+
+class Base:
+
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """constructor
+
+        Args:
+            id (int, None): Defaults to None.
+        """
+        if id is not None:
+            self.id = id
+        else :
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
