fd3d1569b7042b3ca726d4709e49740289b3f3f9
nimrod02 <letrun.a@gmail.com>
2023-06-27 09:47:46 +0200

    add pycodestyle syntax to the project

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -3,7 +3,9 @@
 module square
 """
 
+
 from models.rectangle import Rectangle
+
 
 class Square(Rectangle):
     """Square class
@@ -14,7 +16,7 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """constructor of square
-
+cd mode
         Args:
             size (int): size of Square
             x (int): axe X. Defaults to 0.
@@ -23,8 +25,5 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
-
     def __str__(self):
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
-
-
