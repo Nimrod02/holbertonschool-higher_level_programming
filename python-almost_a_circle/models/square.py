5660ba5fe48a065bd29a95cb7e25c40629d4a3ee
nimrod02 <letrun.a@gmail.com>
2023-06-27 10:01:04 +0200

    assing an argument to each attribute for class rectangle

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -36,3 +36,25 @@ cd mode
     def size(self, value):
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """
+        assigns an argument to each attribute
+        """
+        if args is not []:
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.size = args[1]
+            if len(args) > 3:
+                self.x = args[3]
+            if len(args) > 4:
+                self.y = args[4]
+        if 'id' in kwargs:
+            self.id = kwargs['id']
+        if 'width' in kwargs:
+            self.width = kwargs['width']
+        if 'x' in kwargs:
+            self.x = kwargs['x']
+        if 'y' in kwargs:
+            self.y = kwargs['y']
