c8e3eef008d0850e11ff94d91805db73863cedd6
nimrod02 <letrun.a@gmail.com>
2023-06-27 10:04:08 +0200

    new change id

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -46,10 +46,10 @@ cd mode
                 self.id = args[0]
             if len(args) > 1:
                 self.size = args[1]
+            if len(args) > 2:
+                self.x = args[2]
             if len(args) > 3:
-                self.x = args[3]
-            if len(args) > 4:
-                self.y = args[4]
+                self.y = args[3]
         if 'id' in kwargs:
             self.id = kwargs['id']
         if 'size' in kwargs:
